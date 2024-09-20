61004928b38cf801b8f17cc4b37f247f8f616ece
louisdevzz <vohuunhan1310@gmail.com>
2024-09-20 22:46:23 +0700

    feat: avatar

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react"
 import styles from "./header.module.css"
+import { useState } from "react"
 
 
 export default function Header() {
   const { data: session, status } = useSession()
   const loading = status === "loading"
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev)
+  }
 
   return (
     <>
@@ -57,21 +63,21 @@ export default function Header() {
                       <img alt="Profile picture of a person wearing a red jacket and sunglasses" className="w-10 h-10 rounded-full border border-gray-300" height="40" src={session.user.image} width="40"/>
                     )
                   }
-                  <span className="text-sm font-medium text-gray-700 truncate w-12 p-2 px-3 rounded-full border border-gray-100 shadow-lg">
+                  <span onClick={toggleMenu} className="text-sm font-medium text-gray-700 truncate w-32 p-2 px-3 rounded-full border border-gray-100 shadow-lg">
                       {session?.user?.email ?? session?.user?.name}
                   </span>
                   <div className="px-4 py-2 text-blue-600 bg-blue-100 rounded-full">
                     Verified Human
                   </div>
                 </div>
-                <div className="fixed z-10 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-lg">
+                <div className={`fixed z-10 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-lg ${isOpen ? 'block' : 'hidden'}`}>
                   <div className="flex items-center p-2 space-x-2">
                     {
                       session?.user?.image && (
                         <img alt="Profile picture of a person wearing a red jacket and sunglasses" className="w-10 h-10 rounded-full border border-gray-300" height="40" src={session.user.image} width="40"/>
                       )
                     }
-                    <span className="text-sm font-medium text-gray-700 truncate">
+                    <span  className="text-sm font-medium text-gray-700 truncate">
                       {session?.user?.email ?? session?.user?.name}
                     </span>
                   </div>
