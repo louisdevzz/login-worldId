d6b6c78680f100b81a528bcd483c12cd9b261013
louisdevzz <vohuunhan1310@gmail.com>
2024-09-20 22:39:24 +0700

    feat: frontend

--- components/header.tsx
+++ components/header.tsx
@@ -2,94 +2,120 @@ import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react"
 import styles from "./header.module.css"
 
-// The approach used in this component shows how to build a sign in and sign out
-// component that works on pages which support both client and server side
-// rendering, and avoids any flash incorrect content on initial page load.
+
 export default function Header() {
   const { data: session, status } = useSession()
   const loading = status === "loading"
 
   return (
-    <header>
+    <>
       <noscript>
         <style>{`.nojs-show { opacity: 1; top: 0; }`}</style>
       </noscript>
-      <div className={styles.signedInStatus}>
-        <p
+      <div className='w-full bg-white'>
+        <div
           className={`nojs-show ${
             !session && loading ? styles.loading : styles.loaded
           }`}
         >
           {!session && (
-            <>
-              <span className={styles.notSignedInText}>
-                You are not signed in
-              </span>
-              <a
+            <div className="flex justify-between items-center md:p-4 py-4 max-w-6xl mx-auto">
+              <div className="flex items-center space-x-2">
+                <h1 className="text-3xl font-bold">
+                nada.
+                <span className="text-blue-600">
+                  BOT
+                </span>
+                </h1>
+              </div>
+              <Link 
                 href={`/api/auth/signin`}
-                className={styles.buttonPrimary}
                 onClick={(e) => {
-                  e.preventDefault()
-                  signIn("worldcoin") // when worldcoin is the only provider
-                  // signIn() // when there are multiple providers
+                    e.preventDefault()
+                    signIn("worldcoin")
                 }}
-              >
-                Sign in
-              </a>
-            </>
+                className={styles.button}
+                >
+                <span className={styles.button_top}>Login</span>
+              </Link>
+            </div>
           )}
           {session?.user && (
-            <>
-              {session.user.image && (
-                <span
-                  style={{ backgroundImage: `url('${session.user.image}')` }}
-                  className={styles.avatar}
-                />
-              )}
-              <span className={styles.signedInText}>
-                <small>Signed in as</small>
-                <br />
-                <strong>{session.user.email ?? session.user.name}</strong>
-              </span>
-              <a
-                href={`/api/auth/signout`}
-                className={styles.button}
-                onClick={(e) => {
-                  e.preventDefault()
-                  signOut()
-                }}
-              >
-                Sign out
-              </a>
-            </>
+            <div className="flex justify-between items-center md:p-4 py-4 max-w-6xl mx-auto">
+              <div className="flex items-center space-x-2">
+                <h1 className="text-3xl font-bold">
+                nada.
+                <span className="text-blue-600">
+                  BOT
+                </span>
+                </h1>
+              </div>
+              <div className="relative">
+                <div className="flex items-center space-x-2">
+                  {
+                    session?.user?.image && (
+                      <img alt="Profile picture of a person wearing a red jacket and sunglasses" className="w-10 h-10 rounded-full border border-gray-300" height="40" src={session.user.image} width="40"/>
+                    )
+                  }
+                  <div className="px-4 py-2 text-blue-600 bg-blue-100 rounded-full">
+                    Verified Human
+                  </div>
+                </div>
+                <div className="fixed z-10 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-lg">
+                  <div className="flex items-center p-2 space-x-2">
+                    {
+                      session?.user?.image && (
+                        <img alt="Profile picture of a person wearing a red jacket and sunglasses" className="w-10 h-10 rounded-full border border-gray-300" height="40" src={session.user.image} width="40"/>
+                      )
+                    }
+                    <span className="text-sm font-medium text-gray-700 truncate">
+                    {session?.user?.email ?? session?.user?.name}
+                    </span>
+                  </div>
+                  <div className="p-2">
+                  <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="#">
+                    Visit Near Profile
+                  </Link>
+                  <Link 
+                    className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100" 
+                    href={`/api/auth/signout`}
+                    onClick={(e) => {
+                      e.preventDefault()
+                      signOut()
+                    }}
+                  >
+                    Logout
+                  </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            // <>
+            //   {session.user.image && (
+            //     <span
+            //       style={{ backgroundImage: `url('${session.user.image}')` }}
+            //       className={styles.avatar}
+            //     />
+            //   )}
+            //   <span className={styles.signedInText}>
+            //     <small>Signed in as</small>
+            //     <br />
+            //     <strong>{session.user.email ?? session.user.name}</strong>
+            //   </span>
+            //   <a
+            //     href={`/api/auth/signout`}
+            //     className={styles.button}
+            //     onClick={(e) => {
+            //       e.preventDefault()
+            //       signOut()
+            //     }}
+            //   >
+            //     Sign out
+            //   </a>
+            // </>
           )}
-        </p>
+        </div>
       </div>
-      <nav>
-        <ul className={styles.navItems}>
-          <li className={styles.navItem}>
-            <Link href="/">Home</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/client">Client</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/server">Server</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/protected">Protected</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/api-example">API</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/admin">Admin</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link href="/me">Me</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    </>
   )
 }
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,13 @@
-import Header from "./header"
-import Footer from "./footer"
 import type { ReactNode } from "react"
+import { Space_Grotesk } from 'next/font/google'
+import Header from "./header"
+const SpaceGrotesk = Space_Grotesk({ subsets: ['latin'] , weight: ['400', '500', '700']})
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className={SpaceGrotesk.className}>
       <Header />
       <main>{children}</main>
-      <Footer />
-    </>
+    </div>
   )
 }
